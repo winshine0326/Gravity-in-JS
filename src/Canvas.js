047a074f33f60398c36e7c6407128ef48f080ef4
winshine0326 <shane10340@gmail.com>
2024-10-02 00:45:41 +0900

    스크립트 초기설정

--- src/Canvas.js
+++ src/Canvas.js
@@ -1,7 +1,17 @@
-import React from 'react'
+import React,{ useRef, useEffect} from 'react'
 import Matter from 'matter-js'
 
 function Canvas(){
+    const containerRef = useRef();
+    const canvasRef = useRef();
+    
+    useEffect(()=>{
+        const Engine = Matter.Engine;
+        const World = Matter.World;
+        const Bodies = Matter.Bodies;
+        const engine = Engine.create() //엔진 생성
+        
+    })
     return(
         <div></div>
     );
