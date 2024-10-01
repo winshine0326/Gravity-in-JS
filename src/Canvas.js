8d5c538826c8a26069336d552d882637ee44929d
winshine0326 <shane10340@gmail.com>
2024-10-02 01:04:54 +0900

    렌더 설정

--- src/Canvas.js
+++ src/Canvas.js
@@ -1,19 +1,32 @@
-import React,{ useRef, useEffect} from 'react'
+import React,{ useRef, useEffect } from 'react'
 import Matter from 'matter-js'
 
 function Canvas(){
-    const containerRef = useRef();
     const canvasRef = useRef();
     
     useEffect(()=>{
+        //기초 설정
         const Engine = Matter.Engine;
         const World = Matter.World;
         const Bodies = Matter.Bodies;
+        const Render = Matter.Render
         const engine = Engine.create() //엔진 생성
+        const world = engine.world;
         
+        //렌더 설정
+        const render = Render.create({
+            element: canvasRef.current,
+            engine: engine,
+            options: {
+                width:800,
+                height:600,
+                wireframes:false, //색까지 칠하기
+            }
+        });
     })
     return(
-        <div></div>
+        <div ref={canvasRef}>
+        </div>
     );
 }
 export default Canvas;
