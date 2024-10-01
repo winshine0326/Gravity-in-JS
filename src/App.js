722c28b6108e075ed41df4a0de899c501531fa68
winshine0326 <shane10340@gmail.com>
2024-10-01 22:30:32 +0900

    docs: 파일 정리, 기본세팅

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Canvas from './Canvas';
+
+function App() {
+  return (
+    <>
+      <h1>Matter.Js</h1>
+      <Canvas/>
+    </>
+  );
+}
+
+
+
+export default App;
---
+++ src/Canvas.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import Matter from 'matter-js'
+
+function Canvas(){
+    return(
+        <div></div>
+    );
+}
+export default Canvas;
